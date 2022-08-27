0725ac52deb94a086c437475709d81a58a8c8475
flash-of-genius <amhdaar@gmail.com>
2022-08-27 18:02:38 +0100

    Class component props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import HelloWorld from "./HelloWorld";
 import Message from "./Message";
+import HelloAsClass from "./HelloAsClass";
 
 function App() {
   return (
     <div>
       <HelloWorld who="Amaw" />
       <Message greet="welcome" who="Alienss" />
+      <HelloAsClass who="Earth" />
     </div>
   );
 }
