a094780a82d0109b55b468b06f3a330b763265a2
flash-of-genius <amhdaar@gmail.com>
2022-08-27 17:35:07 +0100

    Multiple props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import HelloWorld from "./HelloWorld";
+import Message from "./Message";
 
 function App() {
   return (
     <div>
       <HelloWorld who="Amaw" />
+      <Message greet="welcome" who="Alienss" />
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,8 @@
+function Message({ greet, who }) {
+  return (
+    <div>
+      {greet}, {who}!!
+    </div>
+  );
+}
+export default Message;
